268350e4e972a8bdc9da42afa5070b35aec87c59
25Braeden <125329807+25Braeden@users.noreply.github.com>
2023-03-09 02:31:30 +0000

    I made the code better

--- primenum.py
+++ primenum.py
@@ -1,13 +1,45 @@
-def is_prime(n):
-    if n < 2:
+"""
+This module provides a function to check if a given number is prime.
+"""
+
+def is_prime(number):
+    """
+    Check if the given number is prime.
+
+    Args:
+        number (int): The number to check.
+
+    Returns:
+        bool: True if the number is prime, False otherwise.
+    """
+    if number < 2:
         return False
-    for i in range(2, n):
-        if n % i == 0:
+    for i in range(2, number):
+        if number % i == 0:
             return False
     return True
 
-x = int(input("Enter a number to see if it's prime: "))
-if is_prime(x):
-    print(f"{x} is a prime number.")
-else:
-    print(f"{x} is not a prime number.")
+
+while True:
+    while True:
+        try:
+            x = int(input("Enter a number to see if it's prime: "))
+            break
+        except ValueError:
+            print("You did not enter a valid number.")
+
+    if is_prime(x):
+        print(f"{x} is a prime number.")
+    else:
+        print(f"{x} is not a prime number.")
+
+    while True:
+        new_num = input("Would you like to see if another number is prime? (Y/N): ").lower()
+        if new_num in ['y', 'n']:
+            break
+        else:
+            print("Invalid input, please enter 'Y' or 'N'.")
+
+    if new_num == 'n':
+        break
+    
